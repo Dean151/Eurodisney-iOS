69387de920dd0b86cc5e78d09f5d8e0fc1521617
Dean151 <thomas.durand.verjat@gmail.com>
2016-03-02 15:46:12 +0100

    Initial Commit

---
+++ EuroDisneyTests/EuroDisneyTests.swift
@@ -0,0 +1,36 @@
+//
+//  EuroDisneyTests.swift
+//  EuroDisneyTests
+//
+//  Created by Thomas Durand on 02/03/2016.
+//  Copyright © 2016 Thomas Durand. All rights reserved.
+//
+
+import XCTest
+@testable import EuroDisney
+
+class EuroDisneyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
